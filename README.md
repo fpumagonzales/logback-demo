c3b5e7980f595d69a11643ea4a09bd032b4e9e37
fpumagonzales <francisco.puma@globant.com>
2022-06-15 18:04:10 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # logback-demo
 Logback Playground
+
+Logger by default
+```
+18:01:11.565 [main] INFO com.example.logback.demo.Main - Hello World
+```
+
---
+++ src/main/java/com/example/logback/demo/Main.java
@@ -0,0 +1,15 @@
+package com.example.logback.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+  
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    logger.info("Hello World");
+
+  }
+
+}
